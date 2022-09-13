46bf1b87063c5460fb54696a9bbdcfdd4c661773
Vartica <varticak1907@gmail.com>
2022-09-13 15:21:43 +0530

    server initial setup

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/vartica",(req,res) => {   //getting the request and calling it
+    res.status(200).send("Hello vartica!");     // response : sending the response to user
+});
+
+app.listen(7000, () => {
+    console.log("Server is listening on port 7000");
+});
+
